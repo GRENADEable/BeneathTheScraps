ed42460242fe042d8c9d8928b4f353859c6a5950
Khatim <kahtim.dar@gmail.com>
2019-08-31 17:54:52 +0400

    Finished the Main Menu Scene

--- BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/SceneManage.cs
+++ BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/SceneManage.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartScene()
     {
-        
+        SceneManager.LoadScene("AR_FINAL");
+        Debug.Log("Scene Started");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
+        Debug.Log("Application Closed");
     }
 }
