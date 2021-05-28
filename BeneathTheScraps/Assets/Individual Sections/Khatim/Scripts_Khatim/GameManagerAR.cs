495b648f8dca59604fea6ac8441b7be716bc66d5
GRENADEable <kahtim.dar@gmail.com>
2021-05-28 16:22:24 +0500

    Added transition from 'AR_Final' to 'Outro' and 'Outro' to 'Menu' Scenes

---
+++ BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerAR.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class GameManagerAR : MonoBehaviour
+{
+    #region Public Variables
+    public Animator fadeBG;
+    public GameObject uiCam;
+    public PlayableDirector introTimeline;
+    public GameObject loadingText;
+    #endregion
+
+    #region Unity Callbacks
+    void Start() => StartCoroutine(ARIntroDelay());
+    #endregion
+
+    #region My Functions
+    public void ARIntroFinished() => StartCoroutine(ARIntroFinishedDelay());
+    #endregion
+
+    #region Coroutines
+    IEnumerator ARIntroDelay()
+    {
+        fadeBG.Play("FadeIn");
+        yield return new WaitForSeconds(0.1f);
+        uiCam.SetActive(false);
+        introTimeline.Play();
+    }
+
+    IEnumerator ARIntroFinishedDelay()
+    {
+        fadeBG.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        loadingText.SetActive(true);
+        SceneManager.LoadScene("OUTRO");
+    }
+    #endregion
+}
---
+++ BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerOutro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class GameManagerOutro : MonoBehaviour
+{
+    #region Public Variables
+    public Animator fadeBG;
+    public PlayableDirector outroTimeline;
+    public GameObject loadingText;
+    #endregion
+
+    #region Unity Callbacks
+    void Start()
+    {
+        fadeBG.Play("FadeIn");
+        outroTimeline.Play();
+    }
+    #endregion
+
+    #region My Functions
+    public void EndOutroFinished() => StartCoroutine(EndOutroFinishedDelay());
+    #endregion
+
+    #region Coroutines
+    IEnumerator EndOutroFinishedDelay()
+    {
+        fadeBG.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        loadingText.SetActive(true);
+        SceneManager.LoadScene("MENU_V2");
+    }
+    #endregion
+}
