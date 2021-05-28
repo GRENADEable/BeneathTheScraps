9d5aa91778f9dd96b614fc8c9d2e9157ea0b9403
GRENADEable <kahtim.dar@gmail.com>
2021-05-28 15:09:20 +0500

    Added transition from intro to ar final scene

--- BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerIntro.cs
+++ BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerIntro.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class GameManagerIntro : MonoBehaviour
 {
     #region Public Variables
     public Animator fadeBG;
     public PlayableDirector introTimeline;
-    #endregion
-
-    #region Private Variables
-
+    public GameObject loadingText;
     #endregion
 
     #region Unity Callbacks
@@ -19,23 +17,23 @@ public class GameManagerIntro : MonoBehaviour
     #endregion
 
     #region My Functions
-    public void IntroFinished()
-    {
-
-    }
-
-    public void OutroFinished()
-    {
-
-    }
+    public void IntroFinished() => StartCoroutine(IntroFinishedDelay());
     #endregion
 
     #region Coroutines
     IEnumerator IntroDelay()
     {
         fadeBG.Play("FadeIn");
-        yield return new WaitForSeconds(0.9f);
+        yield return new WaitForSeconds(0.1f);
         introTimeline.Play();
+    }
+
+    IEnumerator IntroFinishedDelay()
+    {
+        fadeBG.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        loadingText.SetActive(true);
+        SceneManager.LoadScene("AR_FINAL");
     }
     #endregion
 }
--- BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerMenu.cs
+++ BeneathTheScraps/Assets/Individual Sections/Khatim/Scripts_Khatim/GameManagerMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
 public class GameManagerMenu : MonoBehaviour
 {
     #region Public Variables
